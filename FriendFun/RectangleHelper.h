2f9b82f905cf6c5728a6c1eda678c358ba75fb6e
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2022-05-13 09:39:23 -0700

    Add project files.

---
+++ FriendFun/RectangleHelper.h
@@ -0,0 +1,16 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+#include "Rectangle.h"
+
+class RectangleHelper
+{
+public:
+	void modifyRectangle(Rectangle& rect)
+	{
+		rect.length = 500;
+		rect.width = 500;
+	}
+};
+
+#endif
